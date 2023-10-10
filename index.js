9982b0873927f6d3a9544f695a1b2e86d9f729fa
Dominik Beran <d.beran27@gmail.com>
2023-10-10 14:57:24 +0200

    mobile navigation styling & js

---
+++ index.js
@@ -0,0 +1,14 @@
+// when someone clicks the hamburger button
+
+const nav = document.querySelector('.primary-navigation')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+
+navToggle.addEventListener('click', () => {
+
+	const visibility = nav.getAttribute('data-visible')
+	if (visibility === "false") {
+		nav.setAttribute('data-visible', 'true')
+	} else {
+		nav.setAttribute('data-visible', 'false')
+	}
+})
